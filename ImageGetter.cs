83c5e98ef630a9d753a03e43024d35b8ee119c7e
matt-stutzman <112518765+matt-stutzman@users.noreply.github.com>
2025-09-15 12:13:18 -0600

    Add files via upload

---
+++ ImageGetter.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ImageGetter : MonoBehaviour
+{
+    public string url;
+    public Vector3 imagePosition;
+    public Vector3 imageRotation;
+
+    void Start()
+    {
+        StartCoroutine(FetchImage());
+    }
+    
+    IEnumerator FetchImage()
+    {
+        UnityWebRequest imageRequest = UnityWebRequestTexture.GetTexture(url);
+        yield return imageRequest.SendWebRequest();
+
+        if (imageRequest.result == UnityWebRequest.Result.Success)
+        {
+            ///make image
+            Texture2D texture = DownloadHandlerTexture.GetContent(imageRequest);
+            GameObject imageObject = new GameObject("ImageObject");
+            SpriteRenderer renderer = imageObject.AddComponent<SpriteRenderer>();
+            Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f)); ///this line of code is AI generated
+            renderer.sprite = sprite;
+
+            ///image scales
+            int width = texture.width;
+            int height = texture.height;
+
+            float scalar = 0;
+
+            while (width > 1 && height > 1)
+            {
+                width /= 8;
+                height /= 8;
+                scalar++;
+            }
+
+            imageObject.transform.localScale = new Vector3(1f / Mathf.Pow(2, scalar), 1f / Mathf.Pow(2, scalar));
+
+            ///button is placed behind image
+            ButtonManager buttonManager = gameObject.AddComponent<ButtonManager>();
+            buttonManager.imageWidth = texture.width;
+            buttonManager.imageHeight = texture.height;
+
+            var rectTransform = imageObject.AddComponent<RectTransform>();
+            buttonManager.rectTransform = rectTransform;
+            buttonManager.imagePosition = imagePosition;
+            buttonManager.imageRotation = imageRotation;
+            var button = buttonManager.MakeButton(imageObject.transform);
+
+            ///button and image become children of same parent transform
+            imageObject.transform.SetParent(transform.parent, true);
+            button.transform.SetParent(transform.parent, true); 
+
+            ///parent object moves and rotates
+            transform.parent.position = imagePosition;
+            transform.parent.rotation = Quaternion.Euler(imageRotation);
+
+
+            imageRequest.Dispose(); 
+        }
+        else
+        {
+            Debug.LogError($"Unable to fetch the image at url: {url}");
+            Debug.LogError($"{imageRequest.error}");    
+        }
+            
+    }
+}
